f9e468a284a1bb276dc38bfd7b27aba2b171a1bb
Malcolm St. John <malcolm19@hotmail.com>
2021-11-03 17:01:18 -0400

    New spans added and metrics middleware

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -4,13 +4,17 @@ import (
 	"net/http"
 
 	"github.com/Ubivius/microservice-authentication/pkg/handlers"
+	"github.com/Ubivius/pkg-telemetry/metrics"
 	"github.com/gorilla/mux"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
 )
 
 // Mux route handling with gorilla/mux
 func New(authHandler *handlers.AuthHandler) *mux.Router {
 	log.Info("Starting router")
 	router := mux.NewRouter()
+	router.Use(otelmux.Middleware("authentication"))
+	router.Use(metrics.RequestCountMiddleware)
 
 	//Health Check
 	getRouter := router.Methods(http.MethodGet).Subrouter()
